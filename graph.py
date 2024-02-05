e1959a68a57b22bf617567371618d1f0c06f5211
Wilkie Olin-Ammentorp <wolinamm@gmail.com>
2024-02-05 15:39:34 -0600

    updating docs

--- graph.py
+++ graph.py
@@ -1,14 +1,21 @@
 import networkx as nx
 import numpy as np
 import pandas as pd
+import functools
 from scipy.stats import norm
 from scipy.optimize import minimize_scalar
 
+"""
+Placeholder object for a node which doesn't classify (reject) data - only passes it
+"""
 class DummyClassifier:
     def __init__(self):
         self.ratio  = 1
         self.error_matrix = np.array([[0.0, 0.0], [0.0, 1.0]])
 
+"""
+Object to estimate the classification statistics of a processing node based on normal processes
+"""
 class Classifier:
     def __init__(self, ratio, skill, varscale = 1.0):
         self.skill = skill
@@ -44,7 +51,60 @@ class Classifier:
         else:
             print("Solving for classification threshold failed")
             
+"""
+Determine if a node has an active classifier
+"""
+def has_classifier(node):
+    if "classifier" in node.keys():
+        return type(node["classifier"]) is Classifier
+    else:
+        return False
+    
+"""
+Given a node, determine if there's a processing node after it which contains an active classifier
+"""
+def downstream_classifier(graph, node):
+    def traverse(start):
+        down = list(graph.successors(start))
+        
+        if len(down) == 0:
+            return has_classifier(graph.nodes[start])
+        else:
+            return has_classifier(graph.nodes[start]) + functools.reduce(lambda x, y: x + y, map(traverse, down))
+        
+    return traverse(node) > 1
 
+"""
+Find the nodes in a graph with active classifiers
+"""
+def active_classifiers(graph):
+    nodes = [(n, has_classifier(cms_run3.nodes[n])) for n in cms_run3.nodes()]
+    nodes = list(filter(lambda x: x[1], nodes))
+    nodes = [n[0] for n in nodes]
+    return nodes
+
+"""
+Determine the classification contingency table for the entire pipeline
+"""
+def pipeline_contingency(graph):
+    #find the nodes which classify
+    classifiers = active_classifiers(graph)
+    assert len(classifiers) > 0, "No classifiers in processing graph"
+
+    contingency = np.zeros_like(graph.nodes[classifiers[0]]["contingency"])
+    for c in classifiers:
+        #if there's another classifier after this, add only the rejection statistics
+        if downstream_classifier(graph, c):
+            contingency[0,:] += graph.nodes[c]["contingency"][0,:]
+        else:
+            contingency += graph.nodes[c]["contingency"]
+
+    return contingency
+    
+"""
+Given a pandas series that defines a classifier by data reduction and skill,
+convert it to a classifier object or dummy classifier (no classification in node)
+"""
 def entry_to_classifier(entry: pd.core.series.Series):
     skill_u = entry["Skill mean"]
     skill_v = entry["Skill variance"]
@@ -57,10 +117,17 @@ def entry_to_classifier(entry: pd.core.series.Series):
 
     return classifier
 
+
+"""
+Error matrix representing a dummy classifier (no classification)
+"""
 def passing_node():
     error_matrix = np.array([[0.0, 0.0], [0.0, 1.0]])
     return error_matrix
 
+"""
+Return nodes and edges from a dataframe representing the system's data sources (detectors)
+"""
 def detectors(detector_data: pd.DataFrame):
     n = len(detector_data)
     nodes = []
@@ -82,6 +149,9 @@ def detectors(detector_data: pd.DataFrame):
 
     return nodes, edges
 
+"""
+Return nodes and edges from a dataframe representing the system's data processing nodes (triggers)
+"""
 def triggers(trigger_data: pd.DataFrame):
     n = len(trigger_data)
     edges = []
@@ -108,12 +178,20 @@ def triggers(trigger_data: pd.DataFrame):
 
     return triggers, edges
 
+"""
+From a graph, identify the root of the tree (final processing node / storage)
+"""
 def identify_root(graph: nx.classes.digraph):
     od = list(graph.out_degree)
     roots = list(filter(lambda x: x[1] == 0, od))
     assert len(roots) == 1, "More than 1 root identified"
     return roots[0]
 
+
+"""
+Given dataframes representing the system's detectors, processing nodes, and processor scaling estimates,
+construct the graph representing it
+"""
 def construct_graph(detector_data: pd.DataFrame, trigger_data: pd.DataFrame, functions: dict):
     g = nx.DiGraph()
     #add the nodes for detectors
@@ -144,6 +222,9 @@ def construct_graph(detector_data: pd.DataFrame, trigger_data: pd.DataFrame, fun
     return g
 
 
+"""
+Make a copy of a graph without attributes (names only) - for plotting
+"""
 def lean_copy(graph: nx.classes.digraph):
     g = nx.DiGraph()
     for n in list(graph.nodes):
@@ -154,28 +235,28 @@ def lean_copy(graph: nx.classes.digraph):
 
     return g
 
+"""
+Return the negative / positive classification rate from an error matrix
+"""
 def classifier_rate(error_matrix: np.ndarray):
-        """
-        Return the negative / positive classification rate from an error matrix
-        """
+        
         rates = np.einsum("ab,a -> a", error_matrix, np.array([1, 1]))
         return rates / rates.sum()
 
+"""
+Starting from the root node, determine the performance at each classifier node
+"""
 def contingency(graph: nx.classes.digraph):
-    """
-    Starting from the root node, determine the performance at each classifier node
-    """
     def calc_contingency(node):
         this_node = graph.nodes[node]
         this_node["contingency"] = np.round(this_node["error matrix"] * this_node["input rate"])
 
     list(map(calc_contingency, graph.nodes))
 
-
+"""
+Recursively process message passing over the graph to determine the data through each node
+"""
 def message_size(graph: nx.classes.digraph, node: str):
-    """
-    Recursively process message passing over the graph to determine the data through each node
-    """
     inputs = list(graph.predecessors(node))
     input_data = sum([message_size(graph, n) for n in inputs])
     this_node = graph.nodes[node]
@@ -189,10 +270,10 @@ def message_size(graph: nx.classes.digraph, node: str):
     this_node["ops"] = this_node["complexity"](total)
     return total
 
+"""
+Return the rate of messages produced by a node
+"""
 def message_rate(graph: nx.classes.digraph, node: str):
-    """
-    Return the rate of messages produced by a node
-    """
     this_node = graph.nodes[node]
     if "sample rate" in this_node:
         #get the sampling rate of a detector
@@ -209,6 +290,9 @@ def message_rate(graph: nx.classes.digraph, node: str):
     this_node["message rate"] = output_rate
     return output_rate
 
+"""
+For each edge in a graph, calculate data throughput via each link
+"""
 def link_throughput(graph: nx.classes.digraph):
     def calc_throughput(edge):
         input_node = graph.nodes[edge[0]]
@@ -217,6 +301,10 @@ def link_throughput(graph: nx.classes.digraph):
 
     list(map(calc_throughput, graph.edges))
 
+"""
+Given a graph, propagate message sizes, estimate processing requirements, 
+calculate data rates, and produce classification statistics
+"""
 def update_throughput(graph: nx.classes.digraph):
     graph = graph.copy()
     #call the recursive update function on the root node
